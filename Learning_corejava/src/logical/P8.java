4072df237ba9f036a8de3ad07a3c14b2425981a2
chintan <chintan@gmail.com>
2025-03-06 13:42:44 +0530

    all

--- Learning_corejava/src/logical/P8.java
+++ Learning_corejava/src/logical/P8.java
@@ -16,23 +16,146 @@ public class P8 {
 //	   * *
 //	    *
 		
-		int lines = 10;
-		int stars = 1;
-		int spaces = lines-1;
+//		int lines = 11;
+//		int stars = 1;
+//		int spaces = lines-1;
+//		int mid = (lines/2)+1;
+//		
+//		for(int j=1;j<=lines;j++)
+//		{
+//			for(int k=1;k<=spaces;k++)
+//			{
+//				System.out.print(" ");
+//			}
+//			for(int i=1;i<=stars;i++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//			if(j<mid)
+//			{
+//				stars+=2;
+//				spaces--;
+//			}else
+//			{
+//				stars-=2;
+//				spaces++;
+//			}
+//			
+//		}
 		
-		for(int j=1;j<=lines;j++)
+//		int lines = 11;
+//		int stars = 1;
+//		int spaces = lines-1;
+//		int mid = (lines/2)+1;
+//		
+//		for(int j=1;j<=lines;j++)
+//		{
+//			for(int k=1;k<=spaces;k++)
+//			{
+//				System.out.print(" ");
+//			}
+//			for(int i=1;i<=stars;i++)
+//			{
+//				if(i==1 || i==stars)
+//				{
+//					System.out.print("*");
+//				}
+//				else
+//				{
+//					System.out.print(" ");
+//				}
+//			
+//			}
+//			System.out.println();
+//			if(j<mid)
+//			{
+//				stars+=2;
+//				spaces--;
+//			}else
+//			{
+//				stars-=2;
+//				spaces++;
+//			}
+//			
+//		}
+		
+		
+//		1
+//		1 2
+//		1 2 3 
+//		1 2 3 4
+//		1 2 3 4 5
+		
+//		1
+//		2 3
+//		4 5 6
+//		7 8 9 10
+//		11 12 13 14 15
+		
+//		0
+//		1 0
+//		0 1 0
+//		1 0 1 0
+//		0 1 0 1 0
+		
+		
+//		A
+//		B C
+//		D E F
+//		G H I j
+//		K L M N O
+//		
+		
+		
+//		for(int i=1;i<=5;i++)
+//		{
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+		
+//		int k=1;
+//		for(int i=1;i<=5;i++)
+//		{
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.print(k);
+//				k++;
+//			}
+//			System.out.println();
+//		}
+		
+//		for(int i=1;i<=5;i++)
+//		{
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.print((i+j+1)%2);
+//			}
+//			System.out.println();
+//		}
+//		
+		char ch  ='A';
+		for(int i=1;i<=5;i++)
 		{
-			for(int k=1;k<=spaces;k++)
+			for(int j=1;j<=i;j++)
 			{
-				System.out.print(" ");
-			}
-			for(int i=1;i<=stars;i++)
-			{
-				System.out.print("*");
+				System.out.print(ch);
+				ch++;
 			}
 			System.out.println();
-			stars+=2;
-			spaces--;
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
